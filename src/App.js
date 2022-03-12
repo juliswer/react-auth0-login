a178539a187dd56764441ea439ae4e2c7e059bf9
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 18:05:39 -0300

    updating modules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
-import LoginButton from './components/LoginButton';
-import Profile from './components/Profile';
-import LogoutButton from './components/LogoutButton';
+import LoginButton from "./components/LoginButton";
+import Profile from "./components/Profile";
+import LogoutButton from "./components/LogoutButton";
+
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
       <h1>Application</h1>
-      <LoginButton />
-      <LogoutButton />
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
       <Profile />
     </div>
   );
