68f868325e5633ebe1601027bafd9ea363e27c52
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 18:06:44 -0300

    loading component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import LogoutButton from "./components/LogoutButton";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if(isLoading) return <h1>Loading...</h1>;
 
   return (
     <div className="App">
