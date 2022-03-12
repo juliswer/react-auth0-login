c23e43868e41de034d6250937a8560df9efc437a
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 17:55:52 -0300

    new changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {useAuth0} from '@auth0/auth0-react';
+import LoginButton from './LoginButton';
 
 const Profile = () => {
 
     const {user} = useAuth0();
 
+    useEffect(() => {
+        if(!user) return <LoginButton />
+    }, []);
+
   return (
     <div>
-        {JSON.stringify(user)}
+        {user && (
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+                <pre>
+                    {JSON.stringify(user)}
+                </pre>
+            </div>
+        )}
     </div>
   )
 }
