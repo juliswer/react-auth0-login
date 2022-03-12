a787bafecdc7ca6624b44250e32d99745ed04e38
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 17:44:17 -0300

    new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Application</h1>
     </div>
   );
 }
