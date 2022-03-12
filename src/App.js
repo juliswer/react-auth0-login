f7124ee814dcd19e16b12f9f46ebc84fe303558f
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 18:03:32 -0300

    updating modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import LoginButton from './components/LoginButton';
 import Profile from './components/Profile';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
   return (
     <div className="App">
       <h1>Application</h1>
       <LoginButton />
+      <LogoutButton />
       <Profile />
     </div>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import JSONPretty from "react-json-pretty";
+import 'react-json-pretty/themes/monikai.css';
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
